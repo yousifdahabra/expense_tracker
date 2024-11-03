dd15790e33e0e224b68e9b9fa2b8c6eeaed03536
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-11-03 14:57:38 +0200

    Add from, to date and the user can add a note

--- js/transaction.js
+++ js/transaction.js
@@ -16,10 +16,13 @@ const balance_element = document.getElementById("balance");
 const only_incomes_btn = document.getElementById("only_incomes_btn");
 const only_expenses_btn = document.getElementById("only_expenses_btn");
 
+const from_date = document.getElementById("from_date");
+const to_date = document.getElementById("to_date");
+
 let click_incomes_btn = 0
 let click_expenses_btn = 0
 
-const delete_table  = () =>{
+const delete_table_event  = () =>{
     const deletes_btn = document.querySelectorAll(".delete-btn");
     deletes_btn.forEach((delete_btn) => {
         delete_btn.addEventListener("click", (event) => {
@@ -29,12 +32,13 @@ const delete_table  = () =>{
         });
     });
 }
-const edit_table = () =>{
+const edit_table_event = () =>{
 
     const edits_btn = document.querySelectorAll(".edit-btn");
     edits_btn.forEach((edit_btn) => {
         edit_btn.addEventListener("click", (event) => {
             const id = event.target.getAttribute('data-id');
+            const note = event.target.getAttribute('data-note');
             const amount = event.target.getAttribute('data-amount');
             const date = event.target.getAttribute('data-date');
             const transaction_type = event.target.getAttribute('data-transaction_type');
@@ -42,6 +46,7 @@ const edit_table = () =>{
             is_edit.value=id;
             document.getElementById("amount").value = amount;
             document.getElementById("date").value = date;
+            document.getElementById("note").value = note;
             document.getElementById("transaction_type").value = transaction_type ;
 
             form_model.style.display = "block";
@@ -53,10 +58,12 @@ const empty_form = () =>{
     document.getElementById("is_edit").value = 0
     document.getElementById("amount").value = 0;
     document.getElementById("date").value = '';
+    document.getElementById("note").value = '';
     document.getElementById("transaction_type").value = 'incomes';
 }
-const update_table = (ignore = '') =>{
-
+const update_table = (ignore = '',from_date='',to_date='',note='') =>{
+    
+    if(from_date == '' && to_date != '') from_date = new Date().getDate();
     const transactions = JSON.parse(localStorage.getItem('transactions')) || [];
     let body = ``;
     let total_incomes = 0;
@@ -64,7 +71,16 @@ const update_table = (ignore = '') =>{
     let total_balance = 0;
     transactions.forEach((transaction, index) => {
         if(transaction.transaction_type == ignore ) return;
-
+        if(from_date != '' && to_date == '') to_date = new Date().toJSON().slice(0, 10);
+        if(from_date == '' && to_date != '') from_date = new Date().toJSON().slice(0, 10);
+        if(from_date != '' && to_date != ''){
+            if(transaction.date >= from_date && transaction.date <= to_date){
+                
+            }else return
+        }
+        console.log("Update")
+        console.log(from_date)
+        console.log(to_date)
         transaction.amount = parseFloat(transaction.amount);
         if(transaction.transaction_type == 'expenses'){
             total_expenses+=transaction.amount;
@@ -78,10 +94,11 @@ const update_table = (ignore = '') =>{
             <td>${transaction.id}</td>
             <td>${transaction.amount} $</td>
             <td>${total_balance} $</td>
-            <td> ${transaction.transaction_type}</td>
+            <td>${transaction.transaction_type}</td>
+            <td>${transaction.note}</td>
             <td>${transaction.date}</td>
             <td>
-                <button data-id="${transaction.id}" data-transaction_type="${transaction.transaction_type}" data-date="${transaction.date}" data-amount="${transaction.amount}" class="edit-btn view">Edit</button>  
+                <button data-id="${transaction.id}" data-note="${transaction.note}" data-transaction_type="${transaction.transaction_type}" data-date="${transaction.date}" data-amount="${transaction.amount}" class="edit-btn view">Edit</button>  
                 <button data-id="${transaction.id}" class="delete-btn view"  >Delete</button>
             </td>
         </tr>
@@ -93,8 +110,8 @@ const update_table = (ignore = '') =>{
     balance_element.innerHTML = total_balance+" $";
 
     empty_form();
-    delete_table();
-    edit_table();
+    delete_table_event();
+    edit_table_event();
 }
 
 popup_btn.addEventListener("click",()=>{
@@ -113,6 +130,7 @@ close_delete_form_btn.addEventListener("click",()=>{
 submit_form.addEventListener("click", () => {
     const is_edit = document.getElementById("is_edit").value
     const amount = document.getElementById("amount").value || 0;
+    const note = document.getElementById("note").value || '';
     const date = document.getElementById("date").value || new Date().getDate();
     const transaction_type = document.getElementById("transaction_type").value;
     let transactions = JSON.parse(localStorage.getItem('transactions')) || [];
@@ -120,13 +138,13 @@ submit_form.addEventListener("click", () => {
     if(is_edit == 0){
         const id = Math.random().toString(16).slice(2);
         const data = 
-            {"id": id,"amount": amount,"date": date,"transaction_type": transaction_type}
+            {"id": id,"amount": amount,"date": date,"transaction_type": transaction_type,"note": note}
         ;
         transactions.push(data);    
 
     }else{
         const data = 
-            {"id": is_edit,"amount": amount,"date": date,"transaction_type": transaction_type}
+            {"id": is_edit,"amount": amount,"date": date,"transaction_type": transaction_type,"note": note}
         ;
         transactions.forEach((transaction, index) => {
             if(transaction.id == is_edit){
@@ -157,10 +175,21 @@ only_expenses_btn.addEventListener("click",()=>{
     only_expenses_btn.innerHTML= click_incomes_btn === 1 ? "Show All":"Only Expenses";
     update_table(click_expenses_btn === 1 ? 'incomes' : '')
 })
+
 only_incomes_btn.addEventListener("click",()=>{
     click_incomes_btn = 1 - click_incomes_btn;
     only_incomes_btn.innerHTML= click_incomes_btn === 1 ? "Show All":"Only Incomes";
     update_table(click_incomes_btn === 1 ?'expenses' : '')
+})
+
+from_date.addEventListener("change",()=>{
+    console.log('from_date.value',from_date.value)
+    update_table('',from_date.value,to_date.value)
+})
+to_date.addEventListener("change",()=>{
+    console.log('to_date.value',to_date.value)
+
+    update_table('',from_date.value,to_date.value)
 })
 
 
