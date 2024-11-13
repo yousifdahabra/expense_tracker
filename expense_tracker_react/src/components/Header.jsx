4ee7a71ee77fed7f69406e6abaf967c09cb77f22
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-11-13 20:48:56 +0200

    Add header components and Dashboard page use it on App.jsx

---
+++ expense_tracker_react/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <>
+        <header class="flex justify-content-space-between align-items-center second-background-color">
+            <div class="logo-section flex align-items-center">
+                <div class="logo header-text">Expense Tracker</div>
+            </div>
+        </header>
+
+        </>
+    );
+}
+
+export default Header;
