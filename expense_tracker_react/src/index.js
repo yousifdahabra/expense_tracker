748b64d582a46da3721af2b32dc163ae10250d5a
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-11-13 20:32:27 +0200

    Update index.js and App.jsx remove files and code to be empty

--- expense_tracker_react/src/index.js
+++ expense_tracker_react/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from 'App';
+import App from './App';
+import './styles/index.css';
 
 const rootContainer = document.getElementById("root");
 const root = ReactDOM.createRoot(rootContainer);
